2668f2aa7b0214cbc1be8c407f2eeb10db21ffcc
tkathuria <tanuj_kathuria@intuit.com>
2021-01-02 22:45:07 +0530

    first commit

---
+++ src/main/java/com/example/netflixapizuulgatewayserver/NetflixApiZuulGatewayServerApplication.java
@@ -0,0 +1,24 @@
+package com.example.netflixapizuulgatewayserver;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixApiZuulGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApiZuulGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
---
+++ src/test/java/com/example/netflixapizuulgatewayserver/NetflixApiZuulGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netflixapizuulgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApiZuulGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
